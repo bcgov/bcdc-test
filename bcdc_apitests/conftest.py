a1a61bf3f8ff464f4b39e25a03324c680d0edce6
crigdon <craig.rigdon@gov.bc.ca>
2019-07-08 12:15:23 -0700

    DDM-696 - added session_setup_teardown fixture

--- bcdc_apitests/conftest.py
+++ bcdc_apitests/conftest.py
@@ -6,3 +6,13 @@ Created on May 28, 2019
 # this set of fixtures is used by other fixtures and all
 # tests so need to import globally here.
 from bcdc_apitests.fixtures.load_config import *
+
+import pytest
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def session_setup_teardown(request):
+    logger.debug("------------Session Setup--------------")
+    yield
+    logger.debug("-----------Session Teardown--------------")
