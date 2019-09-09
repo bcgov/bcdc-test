5ff6196c8f32b0e3f7294dcb839f5a882644bb45
crigdon <craig.rigdon@gov.bc.ca>
2019-09-09 13:54:01 -0700

    DDM-794 - added users and other mod

---
+++ bcdc_apitests/tests/other/test_other.py
@@ -0,0 +1,14 @@
+'''
+
+Created on Sep 09, 2019
+
+@author: crigdon
+
+'''
+# pylint: disable=invalid-name, unused-argument, too-many-arguments, unused-import
+import logging
+import requests
+import pytest  # @UnusedImport
+
+LOGGER = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
